56828023c01a7317f8944a9e289af7f801f300b8
Artachanidis Christos <46463572+Artachanidisc@users.noreply.github.com>
2019-07-03 02:16:24 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,17 +7,12 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 
 		ObjectiveFunction o1 = new ObjectiveFunction();
-		int nword = 0;
-		int i = 0;
-		int functionSize = 0;
 		ArrayList<String>functionArray=new ArrayList<String>();
 
 		try (Scanner scanner = new Scanner(new File("lp1.txt"));) {
 			while (scanner.hasNext()) {
 				String sent = scanner.nextLine();
 				functionArray.add(sent);
-				nword++;
-			//	System.out.printf("%3d) %s%n", nword, sent);
 			}
 		}
 	o1.testFunction(functionArray);
